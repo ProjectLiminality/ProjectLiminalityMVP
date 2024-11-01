fb3d0797307ab17111c7c47f86732d7c3f40339b
Interface Guy <david.rug98@icloud.com>
2024-11-01 12:31:08 +0000

    refactor: remove unused click event listener on SVG

--- src/components/DreamContent.jsx
+++ src/components/DreamContent.jsx
@@ -157,12 +157,6 @@ const DreamContent = ({ data, onNodeInteraction }) => {
       console.log("DreamContent: SVG background clicked");
     });
 
-    // Add a click event listener to the entire SVG
-    svg.on("click", (event) => {
-      console.log("DreamContent: SVG clicked");
-      event.stopPropagation();
-    });
-
     // Append the text labels.
     const label = svg
       .append("g")
@@ -181,50 +175,7 @@ const DreamContent = ({ data, onNodeInteraction }) => {
     svg.on("click", (event) => zoom(event, root));
     zoomTo([root.x, root.y, root.r * 2]);
 
-    // Zoom functions.
-    function zoomTo(v) {
-      const k = width / v[2];
-
-      view = v;
-
-      label.attr(
-        "transform",
-        (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
-      );
-      node.attr(
-        "transform",
-        (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
-      );
-      node.attr("r", (d) => d.r * k);
-    }
-
-    function zoom(event, d) {
-      const focus0 = focus;
-
-      focus = d;
-
-      const transition = svg
-        .transition()
-        .duration(event.altKey ? 7500 : 750)
-        .tween("zoom", () => {
-          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-          return (t) => zoomTo(i(t));
-        });
-
-      label
-        .filter(function (d) {
-          return d.parent === focus || this.style.display === "inline";
-        })
-        .transition(transition)
-        .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
-        .on("start", function (d) {
-          if (d.parent === focus) this.style.display = "inline";
-        })
-        .on("end", function (d) {
-          if (d.parent !== focus) this.style.display = "none";
-        });
-    }
-  }, [data, onNodeInteraction]);
+  }, [data, onNodeInteraction, selectedNodes]);
 
   return <div ref={ref} style={{ width: '100%', height: '100%' }} />;
 };
