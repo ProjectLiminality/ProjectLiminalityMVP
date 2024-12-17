30a5ef7a252dca433bac20b5d1e0b866b0b48034
Interface Guy <david.rug98@icloud.com>
2024-12-17 20:53:28 +0000

    refactor: Update FullscreenDreamTalk component with color constants and isOpen prop

--- src/components/FullscreenDreamTalk.jsx
+++ src/components/FullscreenDreamTalk.jsx
@@ -1,63 +1,59 @@
-import React from 'react';
-import DreamTalk from './DreamTalk';
-
-const FullscreenDreamTalk = ({ repoName, dreamTalkMedia, metadata, onClose }) => {
-  return (
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.9)',
-      zIndex: 1000,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <div style={{
-        width: '90%',
-        height: '90%',
-        position: 'relative',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <div style={{ width: '80%', height: '80%' }}>
-          <DreamTalk
-            repoName={repoName}
-            dreamTalkMedia={dreamTalkMedia}
-            metadata={metadata}
-            onClick={() => {}}
-            onRightClick={() => {}}
-            onMouseEnter={() => {}}
-            onMouseLeave={() => {}}
-            isHovered={false}
-            borderColor="transparent"
-            onFlip={() => {}}
-            onToggleFullscreen={() => {}}
-          />
-        </div>
-        <button
-          onClick={onClose}
-          style={{
-            position: 'absolute',
-            top: '10px',
-            right: '10px',
-            background: 'rgba(255, 255, 255, 0.5)',
-            color: 'black',
-            border: 'none',
-            borderRadius: '5px',
-            padding: '5px 10px',
-            cursor: 'pointer',
-            zIndex: 1001,
-          }}
-        >
-          Close
-        </button>
-      </div>
-    </div>
-  );
-};
-
+import React from 'react';                                                              
+import { BLACK, BLUE, RED, WHITE } from '../constants/colors';                          
+import DreamTalk from './DreamTalk';                                                    
+                                                                                        
+const FullscreenDreamTalk = ({ isOpen, onClose, repoName, dreamTalkMedia, metadata }) = 
+{                                                                                       
+  if (!isOpen) return null;                                                             
+                                                                                        
+  return (                                                                              
+    <div style={{                                                                       
+      position: 'fixed',                                                                
+      top: 0,                                                                           
+      left: 0,                                                                          
+      width: '100%',                                                                    
+      height: '100%',                                                                   
+      backgroundColor: BLACK,                                                           
+      color: WHITE,                                                                     
+      zIndex: 1000,                                                                     
+      display: 'flex',                                                                  
+      justifyContent: 'center',                                                         
+      alignItems: 'center',                                                             
+    }}>                                                                                 
+      <div style={{                                                                     
+        width: '90%',                                                                   
+        height: '90%',                                                                  
+        position: 'relative',                                                           
+      }}>                                                                               
+        <DreamTalk                                                                      
+          repoName={repoName}                                                           
+          dreamTalkMedia={dreamTalkMedia}                                               
+          metadata={metadata}                                                           
+          onClick={() => {}}                                                            
+          onRightClick={() => {}}                                                       
+          onMouseEnter={() => {}}                                                       
+          onMouseLeave={() => {}}                                                       
+          isHovered={false}                                                             
+          borderColor="transparent"                                                     
+          onFlip={() => {}}                                                             
+          onToggleFullscreen={() => {}}                                                 
+        />                                                                              
+        <button onClick={onClose} style={{                                              
+          position: 'absolute',                                                         
+          top: '10px',                                                                  
+          right: '10px',                                                                
+          padding: '5px 10px',                                                          
+          backgroundColor: RED,                                                         
+          color: WHITE,                                                                 
+          border: 'none',                                                               
+          borderRadius: '4px',                                                          
+          cursor: 'pointer'                                                             
+        }}>                                                                             
+          Close                                                                         
+        </button>                                                                       
+      </div>                                                                            
+    </div>                                                                              
+  );                                                                                    
+};                                                                                      
+                                                                                        
 export default FullscreenDreamTalk;
